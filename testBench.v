7e88e3b25be3619d3ed3f81b73f7280a76072870
alyabozied <abozied@DESKTOP-04IG458.localdomain>
2021-05-26 22:17:48 +0200

    GPU testbench clk fix

--- testBench.v
+++ testBench.v
@@ -7,6 +7,6 @@ wire cnn;
 wire done;
 wire [15:0] data;
 wire [3:0] datain;
-GPU GPU1(rst,clk,interrupt, load, cnn, done, data,datain);
+GPU GPU1(clk,rst,interrupt, load, cnn, done, data,datain);
 IOController IOcontroller(clk, rst, interrupt, load, cnn, done, data, datain, memAdress, memData);
 endmodule
